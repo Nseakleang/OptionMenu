12f9a47eeb0ec058e936768fcc1a043b2c940c27
SC_KPC_06 <SC_KPC_06@KSHRD-KPC-06s-iMac.local>
2018-11-19 00:47:53 -0800

    OptionMenu

--- app/src/main/java/com/seakleang/optionmenu/MainActivity.java
+++ app/src/main/java/com/seakleang/optionmenu/MainActivity.java
@@ -2,6 +2,7 @@ package com.seakleang.optionmenu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -16,6 +17,26 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.option_menu, menu);
+        //search view
+        MenuItem searchItem = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        //register event to searchView
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                MessageHelper.showMessage(MainActivity.this, s);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                MessageHelper.showMessage(MainActivity.this, s);
+                return true;
+            }
+        });
+
+
+
         return true;
     }
 
